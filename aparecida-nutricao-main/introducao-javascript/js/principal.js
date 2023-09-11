99186f69e8e4f42fc7900c7d0b23c44a676ae1b9
vitormanoeloliveiramattos <130366864+vitormanoeloliveiramattos@users.noreply.github.com>
2023-09-11 08:48:33 -0300

    Add files via upload

---
+++ aparecida-nutricao-main/introducao-javascript/js/principal.js
@@ -0,0 +1,14 @@
+//console.log("O arquivo separado está funcionando");
+
+//------------Daqui para baixo começa o codigo
+
+//alteração do titulo
+
+//console.log(document);
+//cria a variavel titulo e armazena o conteudo da classe
+let titulo = document.querySelector(".titulo");
+//console.log(titulo);
+//atribuição de um novo valor na variavel titulo
+titulo.textContent = "Jesus Nutrições!"
+
+//alteração da tabela
